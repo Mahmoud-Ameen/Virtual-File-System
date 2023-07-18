164839a6dbf84984667b8e4aa91996459b378566
Mahmoud <mahmoud612.ashraf@gmail.com>
2023-07-19 01:17:13 +0300

    Implement UserInterface component

--- include/UserInterface.h
+++ include/UserInterface.h
@@ -1,13 +1,73 @@
+#ifndef USERINTERFACE_H
+#define USERINTERFACE_H
 
-#ifndef VIRTUAL_FILE_SYSTEM_USERINTERFACE_H
-#define VIRTUAL_FILE_SYSTEM_USERINTERFACE_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
-class UserInterface{
+#include "FileSystemDataStructure.h"
+#include "FileSystemOperations.h"
 
+/**
+ * @brief The UserInterface class handles user interactions and commands.
+ *
+ * It provides a command line interface for users to interact with the file system.
+ */
+class UserInterface {
 public:
+
+    UserInterface();
+
+    /**
+     * @brief Runs the user interface, accepting user commands and processing them.
+     */
     void run();
+
+
+private:
+    /**
+     * Map of command names to the number of expected arguments.
+     */
+    std::map<std::string,int> argumentsNumber;
+
+    /**
+     * @brief Parses the user command into command type and arguments.
+     * @param command The user command string.
+     * @param commandType Output parameter for the command type.
+     * @param arguments Output parameter for the command arguments.
+     */
+    static void parseCommand(const std::string& inputCommand, std::string& commandType, std::vector<std::string>& arguments);
+
+    /**
+     * @brief Processes the user command.
+     * @param command The user command string.
+     */
+    void processCommand(const std::string& command);
+
+    /**
+     * @brief Validates the user command and its arguments.
+     * @param commandType The command type.
+     * @param args The command arguments.
+     * @return True if the command is valid, false otherwise.
+     */
+    bool validateCommand(const std::string& commandType, std::vector<std::string>& args);
+
+    // Command handlers
+    static void handleChangeDir(const std::vector<std::string>& args);
+    static void handleCreateDir(const std::vector<std::string>& args);
+    static void handleCreateFile(const std::vector<std::string>& args);
+    static void handleRemoveDir(const std::vector<std::string>& args);
+    static void handleRemoveFile(const std::vector<std::string>& args);
+    static void handleRenameFile(const std::vector<std::string>& args);
+    static void handleRenameDir(const std::vector<std::string>& args);
+    static void handleList(const std::vector<std::string>& args);
+    static void handleHierarchyList(const std::vector<std::string>& args);
+    static void handleHelp(const std::vector<std::string>& args);
+
+    /* Helper method to print a tree recursively*/
+    static void printTree(TreeNode* root, int depth);
 
 };
 
-
-#endif //VIRTUAL_FILE_SYSTEM_USERINTERFACE_H
+#endif // USERINTERFACE_H
--- src/UserInterface.cpp
+++ src/UserInterface.cpp
@@ -1,13 +1,242 @@
-#pragma clang diagnostic push
-#pragma ide diagnostic ignored "readability-convert-member-functions-to-static"
-#include <iostream>
+#include <algorithm>
 
 #include "UserInterface.h"
+#include "ErrorHandling.h"
+#include "FileSystemOperations.h"
 
-using namespace std;
+UserInterface::UserInterface(){
 
-void UserInterface::run() {
-    cout << "Hello World\n";
+    argumentsNumber["help"] = 0;
+    argumentsNumber["ls"] = 0;
+    argumentsNumber["lsh"] = 0;
+    argumentsNumber["cd"] = 1;
+    argumentsNumber["mkdir"] = 1;
+    argumentsNumber["touch"] = 1;
+    argumentsNumber["rm"] = 1;
+    argumentsNumber["rmdir"] = 1;
+    argumentsNumber["rename"] = 2;
+    argumentsNumber["renameDir"] = 2;
 }
 
-#pragma clang diagnostic pop
+/** Public methods */
+void UserInterface::run() {
+
+    while (true) {
+        // Read user input
+        std::string command;
+
+        std::cout <<FileSystemOperations::getCurrentPath() << "> ";
+        std::getline(std::cin, command);
+
+        // Check for exit command
+        if (command == "exit") {
+            break;
+        }
+
+        processCommand(command);
+    }
+}
+
+/** Private Methods **/
+
+bool UserInterface::validateCommand(const std::string& commandType,std::vector<std::string>& args){
+    if(UserInterface::argumentsNumber.find(commandType) == UserInterface::argumentsNumber.end())
+        return true;
+
+    return (UserInterface::argumentsNumber[commandType] == args.size());
+}
+
+void UserInterface::processCommand(const std::string &inputCommand){
+    // parse the command
+    std::string commandType;
+    std::vector<std::string> args;
+    parseCommand(inputCommand,commandType,args);
+
+    if(!validateCommand(commandType,args)){
+        std::cout <<"Number of arguments doesn't match command\n";
+        return;
+    }
+
+    if(commandType == "cd")
+        handleChangeDir(args);
+
+    else if(commandType == "mkdir")
+        handleCreateDir(args);
+
+    else if (commandType =="touch")
+        handleCreateFile(args);
+
+    else if (commandType == "ls")
+        handleList(args);
+
+    else if (commandType == "rmdir")
+        handleRemoveDir(args);
+
+    else if (commandType == "rm")
+        handleRemoveFile(args);
+
+    else if (commandType == "rename")
+        handleRenameFile(args);
+
+    else if (commandType == "renameDir")
+        handleRenameDir(args);
+
+    else if (commandType == "lsh")
+        handleHierarchyList(args);
+
+    else if (commandType == "help")
+        handleHelp(args);
+
+    else
+        std::cout << "Unknown Command. For details about supported commands type 'help'\n";
+}
+
+std::string trimString(const std::string& str) {
+    std::string trimmedStr = str;
+    // Trim spaces from the left side
+    trimmedStr.erase(trimmedStr.begin(), std::find_if(trimmedStr.begin(), trimmedStr.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
+
+    // Trim spaces from the right side
+    trimmedStr.erase(std::find_if(trimmedStr.rbegin(), trimmedStr.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), trimmedStr.end());
+
+    return trimmedStr;
+}
+
+void UserInterface::parseCommand(const std::string &inputCommand, std::string &commandType,
+                                 std::vector<std::string> &arguments) {
+
+    std::string command = trimString(inputCommand);
+
+    // get command type
+    commandType = "";
+    int lastProcessedIndex = -1;
+    for (int i = 0; i < command.size(); i++) {
+        lastProcessedIndex = i;
+        char ch = command[i];
+
+        if(ch == ' ' && !commandType.empty()) {
+            break;
+        }
+        commandType.push_back(ch);
+    }
+
+    // checks if the command is fully processed
+    if(lastProcessedIndex + 1 >= command.size()) return;
+
+    arguments.clear();
+    std::string argument;
+    for (int i = lastProcessedIndex + 1; i < command.size(); ++i) {
+        if(command[i] == ' '){
+            if(!argument.empty()) {
+                arguments.push_back(argument);
+                argument = "";
+            }
+            continue;
+        }
+
+        argument.push_back(command[i]);
+    }
+    if(!argument.empty()) arguments.push_back(argument);
+
+}
+
+void UserInterface::handleChangeDir(const std::vector<std::string> &args) {
+    FileSystemOperations::changeDirectory(args[0]);
+}
+void UserInterface::handleCreateDir(const std::vector<std::string> &args) {
+    FileSystemOperations::createDirectory(args[0]);
+}
+void UserInterface::handleCreateFile(const std::vector<std::string> &args) {
+    FileSystemOperations::createFile(args[0]);
+}
+void UserInterface::handleList(const std::vector<std::string> &args) {
+    auto dirs = FileSystemOperations::getDirectoriesNames();
+    auto files = FileSystemOperations::getFilesNames();
+
+    std::string space = "     ";
+    for (const auto & dir : dirs) {
+        std::cout << "*" << dir << '\n';
+    }
+    for (const auto & file : files) {
+        std::cout << file << '\n';
+    }
+}
+void UserInterface::handleRemoveDir(const std::vector<std::string> &args) {
+    FileSystemOperations::removeDirectory(args[0]);
+}
+void UserInterface::handleRemoveFile(const std::vector<std::string> &args) {
+    FileSystemOperations::removeFile(args[0]);
+}
+void UserInterface::handleRenameFile(const std::vector<std::string> &args){
+    FileSystemOperations::renameFile(args[0],args[1]);
+}
+void UserInterface::handleRenameDir(const std::vector<std::string> &args){
+    FileSystemOperations::renameDir(args[0],args[1]);
+}
+void UserInterface::handleHierarchyList(const std::vector<std::string> &args) {
+    auto tree = FileSystemOperations::getTreePresentation();
+    printTree(tree,0);
+}
+
+void UserInterface::printTree(TreeNode* root, int depth){
+    for (int i = 0; i < depth; ++i) {
+        std::cout<<"--";
+    }
+    std::cout<<"\\" + root->name << std::endl;
+    for (auto child:root->children) {
+        printTree(child,depth+1);
+    }
+}
+
+void UserInterface::handleHelp(const std::vector<std::string> &args) {
+    std::cout <<"Supported Commands List:\n"
+                "1- mkdir [directoryName]\n"
+                "creates a directory named [directoryName].\n"
+                "directoryName must not contain /, \\ or spaces\n"
+                "\n"
+
+                "2- rmdir [directoryName]\n"
+                "removes the directory named [directoryName] in current directory\n"
+                "\n"
+
+                "3- cd [path]\n"
+                "changes current directory to the specified path.\n"
+                "path '..' refers to the parent of current directory.\n"
+                "example of an accepted path '../mahmoud/desktop'\n"
+                "\n"
+
+                "4- touch [fileName]\n"
+                "creates a file named [fileName]\n"
+                "fileName must follow the form '[name].[extension]'\n"
+                "fileName must not contain any /, \\ or spaces\n"
+                "\n"
+
+                "5- rm [fileName]\n"
+                "removes the file named [fileName] in current directory\n"
+                "\n"
+
+                "6- rename [initialFileName] [newFileName]\n"
+                "rename the file named [initialFileName] in current directory to [newFileName].\n"
+                "\n"
+
+                "7- renameDir [initialDirName] [newDirName]\n"
+                "rename the Dir named [initialDirName] in current directory to [newDirName].\n"
+                "\n"
+
+                "8- ls\n"
+                "lists all directories and files in current directory.\n"
+                "directories are prefixed with an asterisk.\n"
+                "\n"
+
+                "9- lsh\n"
+                "shows the folders hierarchy.\n"
+                "lists all directories and files in current directory and for each directory, its content is listed too.\n"
+                "\n"
+
+                "10- help\n"
+                "shows this info message.\n";
+}
